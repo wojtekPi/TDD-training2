e34a6a0bdcbb3369f671b6be644a5e242f024985
Grzegorz <cagaragrzegorz@gmail.com>
2017-08-10 19:51:12 +0200

    new added

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,19 @@
 
 public class StringCalculator {
     public int Add(String input){
-        String[] pattern = input.split(",");
-        int result = 0;
-
-        for(int i =0; i < pattern.length; i++){
-            result += Integer.parseInt(pattern[i]);
-        }
         if(input.length()==1){
             return Integer.parseInt(input);
         }
         if(input.length()==0){
             return 0;
         }
+        String[] pattern = input.split(",");
+        int result = 0;
+
+        for(int i =0; i < pattern.length; i++){
+            result += Integer.parseInt(pattern[i]);
+        }
+
         return result;
     }
 }
