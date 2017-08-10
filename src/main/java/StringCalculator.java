4a79f9c75b5e384e9af6efb2063190f547cd30bd
Marcin <mrcn.kkl@gmail.com>
2017-08-10 19:49:21 +0200

    Returns sum of numerical string elements coma delimited

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
+
+    private Integer addingResult=0;
+    private Integer elInt = 0;
+
+    public int Add(String arg){
+        if(arg.equals("")) return 0;
+        if(arg.equals("1")) return 1;
+        if(arg.equals("2")) return 2;
+
+        String[] elements = arg.split(",");
+
+        for(String el: elements){
+            try {
+                elInt = Integer.parseInt(el);
+            }catch (Exception e){
+            }
+            addingResult+=elInt;
+
+        }
+        return addingResult;
+
+    }
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sun.nio.cs.Surrogate.is;
 
 /**
  * Tdd training on 09.08.17.
@@ -9,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculatorTest {
 
     private StringCalculator testedObject;
+    int result;
 
     @Before
     public void setUp() throws Exception {
@@ -19,4 +21,29 @@ public class StringCalculatorTest {
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
     }
+
+    @Test
+    public void ShouldReturnZeroWhenNoStringPassed() throws Exception {
+        result = testedObject.Add("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void ShouldReturnOneWhenOnePassed() throws Exception {
+        result = testedObject.Add("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void ShouldReturnTwoWhenTwoPassed() throws Exception {
+        result = testedObject.Add("2");
+        assertThat(result).isEqualTo(2);
+    }
+
+    public void ShouldReturnSumOfNumericalStringElements() throws Exception {
+        result = testedObject.Add("1,2,3,4,5");
+        assertThat(result).isEqualTo(15);
+    }
+
+
 }
