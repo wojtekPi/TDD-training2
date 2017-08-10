521ee1248ff36b7648469fe461db5182aed99ea0
Grzegorz Maciborek <g.maciborek@wp.pl>
2017-08-10 20:26:19 +0200

    poprawka

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,20 @@
+import org.assertj.core.internal.Characters;
 
 public class StringCalculator {
+    public int Add(String input) {
+        if (input.equals("")) {
+            return 0;
+        } else if (input.equals("1")) {
+            return 1;
+        }
+
+        String[] words = input.split(",");
+        int result = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            result += Integer.parseInt(words[i]);
+        }
+        return result;
+
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,11 +1,13 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tdd training on 09.08.17.
  */
+
 public class StringCalculatorTest {
 
     private StringCalculator testedObject;
@@ -19,4 +21,28 @@ public class StringCalculatorTest {
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
     }
+
+    @Test
+    public void shouldReturnZeroWhenEmptyStringPassed () throws Exception {
+        int result = testedObject.Add("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnOneWhenOnePassed () throws Exception {
+        int result = testedObject.Add("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnTwoWhenTwoPassed() throws Exception {
+        int result = testedObject.Add("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+    @Test
+    public void shouldReturnNumbersWhenNPassed() throws Exception {
+        int result = testedObject.Add("1,2,3,4");
+        assertThat(result).isEqualTo(10);
+    }
+
 }
