eb77d776abf2f3e772c81423cd27547aae2884b0
Tomasz <t.treppa@gmail.com>
2017-08-10 20:57:32 +0200

    With JunitParams

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,11 +1,17 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tdd training on 09.08.17.
  */
+
+
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
     private StringCalculator testedObject;
@@ -66,16 +72,31 @@ public class StringCalculatorTest {
 
     }
 
-    @Test
-    public void shouldReturnTenWhen_7h3Passed() {
-        int result = testedObject.Add("7h3");
-        assertThat(result).isEqualTo(10);
+//    @Test
+//    public void shouldReturnTenWhen_7h3Passed() {
+//        int result = testedObject.Add("7h3");
+//        assertThat(result).isEqualTo(10);
+//    }
+//
+//    @Test
+//    public void shouldReturnTenWhen1hf_coma_dot_3ds6Passed() {
+//        int result = testedObject.Add("1h,.3ds6");
+//        assertThat(result).isEqualTo(10);
+//    }
+
+    private Object[][] parameterForshouldReturnCorrectValueForSpecificInput(){
+        return new Object[][]{
+                {"7h3", 10},
+                {"1h,.3ds6", 10}
+        };
     }
 
+
     @Test
-    public void shouldReturnTenWhen1hf_coma_dot_3ds6Passed() {
-        int result = testedObject.Add("1h,.3ds6");
-        assertThat(result).isEqualTo(10);
+    @Parameters (method = "parameterForshouldReturnCorrectValueForSpecificInput")
+    public void shouldReturnCorrectValueForSpecificInput(String input, int expectedResult){
+      int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
 }
