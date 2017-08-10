a986f21dc72044180d30b4c4d6805274cfa4aea4
Michał Folwarski <folwarski.michal@gmial.com>
2017-08-10 19:56:01 +0200

    supported delimiters: comma and new line

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+    public int Add(String input) {
+        if (input.length() == 0) {
+            return 0;
+        }
+
+        int result = 0;
+
+        Pattern pattern = Pattern.compile("(\\d+)[,\\n]*");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            String number = matcher.group(1);
+            result += Integer.parseInt(number);
+        }
+
+        return result;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Tdd training on 09.08.17.
@@ -18,5 +19,42 @@ public class StringCalculatorTest {
     @Test
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnZeroWhenEmptyStringPassed(){
+        int result = testedObject.Add("");
+        //assertEquals(0,result);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnOneWhenOnePassed() throws Exception {
+        int result = testedObject.Add("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnTwoWhenTwoPassed() throws Exception {
+        int result = testedObject.Add("2");
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldReturnThreeWhenOneAndTwoPassed() throws Exception {
+        int result = testedObject.Add("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnTenWhenOneTwoSevenPassed() throws Exception {
+        int result = testedObject.Add("1,2,7");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    public void shouldReturnSixWhenNewLineisBetwenPassed() throws Exception {
+        int result = testedObject.Add("1\n2,3");
+        assertThat(result).isEqualTo(6);
     }
 }
