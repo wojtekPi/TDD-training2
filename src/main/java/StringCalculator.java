5c2fb2491640fa1ba72a104d02a57042dd502a3a
Lidka <lidia.kucab@gmail.com>
2017-08-10 20:19:42 +0200

    new version with ADD

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,18 @@
-
 public class StringCalculator {
+
+    public int Add(String input) {
+        if (input.equals("1")) {
+            return 1;
+        }
+        if (input.equals("")) {
+            return 0;
+        }
+        input = input.replace("\n",",");
+        String[] words = input.split(",");
+        int result = 0;
+        for (int i = 0; i < words.length; i++) {
+            result += Integer.parseInt(words[i]);
+        }
+        return result;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -18,5 +19,47 @@ public class StringCalculatorTest {
     @Test
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
+    }
+
+    @Test
+    public void ShouldReturnZeroWhenEmptyStringPassed() {
+        int result = testedObject.Add("");
+        assertEquals(result, 0);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnOneWhenOnePassed() throws Exception {
+        int result = testedObject.Add("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnSumWhenTwoPassed() throws Exception {
+        int result = testedObject.Add("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void shouldReturnSumWhenUnknownAmountOfNumbers() throws Exception {
+        int result = testedObject.Add("1,2,3,4");
+        assertThat(result).isEqualTo(10);
+    }
+    @Test
+    public void shouldReturnSumWhennewLinesBetweenNumbers() throws Exception {
+        int result = testedObject.Add("1\n2,3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void shouldtReturnSumWhennewLinesBetweenNumbers() throws Exception {
+        int result = testedObject.Add("1,\n");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnSumWhenNewDifferentDelimiters() throws Exception {
+        int result = testedObject.Add("//;\\n1;2");
+        assertThat(result).isEqualTo(1);
     }
 }
