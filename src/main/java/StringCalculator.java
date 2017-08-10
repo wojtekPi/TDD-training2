6dae0b31ab1a0b4525e350499851c45d43149a6a
Filip Bielicki <filip.lk.bielicki@gmail.com>
2017-08-10 20:12:39 +0200

    fbielicki next commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,6 @@
 
 public class StringCalculator {
     public int Add(String input) {
-        if (input.equals("1")) {
-            return 1;
-        }
-        else if(input.equals("1,2")){
-            return 3;
-        }
 
         int sum = 0;
         for(int i = 0; i < input.length(); i++){
