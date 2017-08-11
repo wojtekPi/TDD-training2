5a005146ca74db6f25a0072fdab51effcc2ed7d9
Filip Bielicki <filip.lk.bielicki@gmail.com>
2017-08-11 20:57:10 +0200

    fbielicki next commit

--- src/test/java/bank/PaymentServiceTest.java
+++ src/test/java/bank/PaymentServiceTest.java
@@ -3,7 +3,9 @@ package bank;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,6 +15,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(JUnitParamsRunner.class)
 public class PaymentServiceTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private PaymentService testedObject;
 
@@ -25,9 +30,6 @@ public class PaymentServiceTest {
         return new Object[][]{
                 {100,2,30,70,32},
                 {10,50,50,-40,100},
-
-
-
         };
     }
 
@@ -38,16 +40,67 @@ public class PaymentServiceTest {
                                                            int moneyToTransfer, int expectedBalanceOfAccountOneAfterTransfer, int expectedBalanceOfAccountTwoAfterTransfer)
             throws Exception {
         Account accountOne = new Account();
+        Instrument instrumentOne = new Instrument();
         accountOne.setId(1);
-        accountOne.setBalance(accountOneBalance);
+        instrumentOne.setAmount(accountOneBalance);
+        accountOne.setBalance(instrumentOne);
+
 
         Account accountTwo = new Account();
+        Instrument instrumentTwo = new Instrument();
         accountTwo.setId(2);
-        accountTwo.setBalance(accountTwoBalance);
+        instrumentTwo.setAmount(accountTwoBalance);
+        accountTwo.setBalance(instrumentTwo);
 
-        testedObject.transferMoney(accountOne,accountTwo,moneyToTransfer);
+        Instrument instrumentToTransfer = new Instrument();
 
-        assertThat(accountOne.getBalance()).isEqualTo(expectedBalanceOfAccountOneAfterTransfer);
-        assertThat(accountTwo.getBalance()).isEqualTo(expectedBalanceOfAccountTwoAfterTransfer);
+        instrumentToTransfer.setAmount(moneyToTransfer);
+
+        testedObject.transferMoney(accountOne,accountTwo,instrumentToTransfer);
+
+        assertThat(accountOne.getBalance().getAmount()).isEqualTo(expectedBalanceOfAccountOneAfterTransfer);
+        assertThat(accountTwo.getBalance().getAmount()).isEqualTo(expectedBalanceOfAccountTwoAfterTransfer);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenBalanceTooLow(){
+        Account accountOne = new Account();
+        Instrument instrumentOne = new Instrument();
+        instrumentOne.setAmount(499);
+        instrumentOne.setCurrency(Currency.PLN);
+        accountOne.setId(1);
+        accountOne.setBalance(instrumentOne);
+
+        Account accountTwo = new Account();
+        Instrument instrumentTwo = new Instrument();
+        instrumentTwo.setAmount(200);
+        instrumentTwo.setCurrency(Currency.PLN);
+        accountTwo.setId(2);
+        accountTwo.setBalance(instrumentTwo);
+
+        Instrument instrumentToTransfer = new Instrument();
+        instrumentToTransfer.setAmount(1000);
+        instrumentToTransfer.setCurrency(Currency.PLN);
+
+
+        testedObject.transferMoney(accountOne,accountTwo,instrumentToTransfer);
+    }
+
+//    //mozliwosc sprawdzenia komunikatu rzucanego przez wyjatek (+adnotacja Rule u gory klasy)
+//    @Test
+//    public void shouldThrowExceptionWhenBalanceTooLow(){
+//        thrown.expect(IllegalArgumentException.class);
+//        thrown.expectMessage("I'm very sorry, but you don't have enough money...");
+//
+//
+//        Account accountOne = new Account();
+//        accountOne.setId(1);
+//        accountOne.setBalance(0);
+//
+//        Account accountTwo = new Account();
+//        accountTwo.setId(2);
+//        accountTwo.setBalance(200);
+//
+//        testedObject.transferMoney(accountOne,accountTwo,1000);
+//    }
 }
