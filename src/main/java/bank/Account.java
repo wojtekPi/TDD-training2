70d2122990f2d43d39b47a51239bae5900bbe7c2
Łukasz Witek <lukasz.witek123@gmail.com>
2017-08-11 19:16:09 +0200

    testBank

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -4,4 +4,24 @@ package bank;
  * Tdd training on 11.08.17.
  */
 public class Account {
+
+    private int id;
+    private int balance;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
--- src/test/java/bank/AccountTest.java
+++ src/test/java/bank/AccountTest.java
@@ -3,6 +3,7 @@ package bank;
 import org.junit.Before;
 import org.junit.Test;
 
+import static bank.PaymentService.transferMoney;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -11,14 +12,41 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class AccountTest {
     private Account testedObject;
+    private Account testedObject2;
 
     @Before
     public void setUp() throws Exception {
         testedObject = new Account();
+
     }
 
     @Test
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnWhenIDValuePassed() {
+        testedObject.setId(123456789);
+        int id = testedObject.getId();
+        assertThat(id).isEqualTo(123456789);
+    }
+
+    @Test
+    public void shouBalanceReturnWhenBalanceValuePassed() {
+        testedObject.setBalance(10000);
+        int id = testedObject.getBalance();
+        assertThat(id).isEqualTo(10000);
+    }
+
+    @Test
+    public void shouldTransferMoneyFromOneAccountToTheOther() {
+        testedObject2 = new Account();
+
+        testedObject.setBalance(2000);
+        testedObject2.setBalance(1000);
+        transferMoney(500, testedObject, testedObject2);
+        assertThat(testedObject.getBalance()).isEqualTo(1500);
+        assertThat(testedObject2.getBalance()).isEqualTo(1500);
     }
 }
